50963aefe50557a3e03494d6a8ddee7bfa15a8df
irfan <shaikirfan225@gmail.com>
2019-01-09 18:52:47 +0530

    completed user interface theam and other issues

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,9 +1,7 @@
 package net.kzn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,7 +10,8 @@ public class PageController {
 	@RequestMapping(value= {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greetings", "welcome to Spring Web-Mvc");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
@@ -26,13 +25,30 @@ public class PageController {
 //		return mv;
 //	}
 	
-	@RequestMapping(value="/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting")String greeting) {
-		if(greeting==null) {
-			greeting="Hello immu";
-		}
+	
+//	@RequestMapping(value="/test/{greeting}")
+//	public ModelAndView test(@PathVariable("greeting")String greeting) {
+//		if(greeting==null) {
+//			greeting="Hello immu";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", "greeting");
+//		return mv;
+//	}
+	
+	@RequestMapping(value= {"/about"})
+	public ModelAndView about() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "greeting");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value= {"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact");
+		mv.addObject("userClickContact", true);
 		return mv;
 	}
 }
---
+++ onlineshopping/src/main/webapp/WEB-INF/views/shared/footer.jsp
@@ -0,0 +1,11 @@
+<div class="container footer" style="height: 60px; ">
+	<!-- Footer -->
+	<footer>
+		<div class="row">
+			<div class="col-lg-12" style="height: 43px; ">
+				<p>Copyright &copy; Online Shopping 2019 - Designed and
+					Developed By Irfan@Jaan</p>
+			</div>
+		</div>
+	</footer>
+</div>
